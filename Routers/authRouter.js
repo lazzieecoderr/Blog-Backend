db369475b6d592200d5f0ce467c1cfd3b9673923
lazzieecoderr <chakravarthyrrc@gmail.com>
2024-06-05 13:34:21 +0530

    Day 2 completed

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { registerUser } from '../Controllers/authController.js';
+import { google, loginUser, registerUser } from '../Controllers/authController.js';
 
 
 
 const router = express.Router();
 
 router.post("/register-user",registerUser)
+router.post("/login-user",loginUser)
+router.post("/google",google)
 
 
 
